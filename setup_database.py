5c64c04b7b00481bb05aaa4e36c48aa06d69f8f6
Moses Wan <wanmoses1@gmail.com>
2013-06-20 18:24:41 +0000

    Updated setup_database.py before leaving to home

--- setup_database.py
+++ setup_database.py
@@ -6,8 +6,24 @@ conn = sqlite3.connect('test.db')
 #
 # This will keep track of the entry it will next write to in the
 # given table
-conn.execute('''CREATE TABLE IF NOT EXISTS row_counters (
-	table_name TEXT PRIMARY KEY,
-	counter INTEGER);''')
+conn.execute("CREATE TABLE IF NOT EXISTS row_counters ( \
+	table_name TEXT PRIMARY KEY,\
+	counter INTEGER);")
 
+conn.execute("INSERT INTO row_counters VALUES('hour_record', 20);")
+conn.commit()
+
+# Forming hour_counters table
+#
+# This will keep track of energy used in hourly blocks
+conn.execute("CREATE TABLE IF NOT EXISTS hour_record (\
+	rowID INTEGER,\
+	time TIMESTAMP,\
+	energy REAL);")
+
+for i in range(60):
+	conn.execute("INSERT INTO hour_record VALUES (\
+		" + str(i+1) + ", NULL, NULL );")
+
+conn.commit()
 conn.close()
