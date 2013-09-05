4fe521dff1e264ef8d8d152b9590cfc640eb04d4
Moses Wan <wanmoses1@gmail.com>
2013-09-05 19:18:31 +1000

    Transfer to main board

--- database_command.py
+++ database_command.py
@@ -8,13 +8,6 @@ def setup():
 	cron.write()
         job = cron.new(command='python /home/pi/sample_power/database_command.py hourly')
         job.minute.on(0)
-        job = cron.new(command='python /home/pi/sample_power/database_command.py daily')
-        job.hour.on(0)
-        job.minute.on(0)
-        job = cron.new(command='python /home/pi/sample_power/database_command.py monthly')
-        job.dom.on(1)
-        job.hour.on(0)
-        job.minute.on(0)
         cron.write()
         
 	conn = sqlite3.connect('test.db')
@@ -32,8 +25,6 @@ def setup():
 		counter INTEGER);")
 
 	conn.execute("INSERT INTO row_counters VALUES ('real_time_record', 1)")
-	conn.execute("INSERT INTO row_counters VALUES ('last_24_hours', 1)")
-	conn.execute("INSERT INTO row_counters VALUES ('last_31_days', 1)")
 
 	# Forming real time record
 	#
@@ -51,114 +42,27 @@ def setup():
 			" + str(i+1) + ", datetime(CURRENT_TIMESTAMP),\
                          0, 0 );")
 
-	# Forming record of last 24 hours
+	# Hourly
 	#
-	# This will keep track of energy used in last 24 hours
-	# and updates in hourly blocks
+	# This will keep track of energy used in hourly blocks
 	print "Creating 'last_24_hours' and filling it with 24 dummy rows"
 	conn.execute("DROP TABLE IF EXISTS last_24_hours;")
-	conn.execute("CREATE TABLE IF NOT EXISTS last_24_hours (\
+	conn.execute("CREATE TABLE IF NOT EXISTS hourly_record (\
 		rowID INTEGER,\
 		time TIMESTAMP,\
 		energy REAL);")
 
-	for i in range(24):
-		conn.execute("INSERT INTO last_24_hours VALUES (\
-			" + str(i+1) + ", datetime(CURRENT_TIMESTAMP),\
-                        0);")
-
-
-        # Forming record of last 31 days
-	#
-	# This will keep track of energy used in the last 31 days
-	# and updates in daily blocks
-	print "Creating 'last_31_days' and filling it with 31 dummy rows"
-	conn.execute("DROP TABLE IF EXISTS last_31_days;")
-	conn.execute("CREATE TABLE IF NOT EXISTS last_31_days (\
-		rowID INTEGER,\
-		time TIMESTAMP,\
-		energy REAL);")
-
-	for i in range(31):
-		conn.execute("INSERT INTO last_31_days VALUES (\
-			" + str(i+1) + ", datetime(CURRENT_TIMESTAMP\
-                        ), 0);")
-
-        # Monthly record of energy usage
-        #
-        # This record will be continuously growning
-        conn.execute("DROP TABLE IF EXISTS monthly_record;")
-        conn.execute("CREATE TABLE IF NOT EXISTS monthly_record (\
-                time TIMESTAMP,\
-                energy REAL);")
-        
 	conn.commit()
 	conn.close()
 
 def hourly():
-
         conn = sqlite3.connect('test.db')
-
         cur = conn.cursor()
-        cur.execute("SELECT counter FROM row_counters WHERE (\
-                table_name = 'last_24_hours');")
-
-        counter = cur.fetchone()[0]
-
         cur.execute("SELECT SUM(energy) FROM real_time_record WHERE (time > datetime(CURRENT_TIMESTAMP, '-1 hour'));")
 
         energy_total = cur.fetchone()[0]
 
-        conn.execute("UPDATE last_24_hours SET energy = " + str(energy_total) + ", \
-                time = datetime(CURRENT_TIMESTAMP) \
-                WHERE (rowID = " + str(counter) + ");")
-        
-        if counter >= 24:
-                conn.execute("UPDATE row_counters SET counter = 1 \
-                        WHERE (table_name = 'last_24_hours');")
-        else:
-                conn.execute("UPDATE row_counters SET counter = " + str(counter + 1) + " \
-                        WHERE (table_name = 'last_24_hours');")
-        
-        conn.commit()
-        conn.close()
-
-def daily():
-
-        conn = sqlite3.connect('test.db')
-
-        cur = conn.cursor()
-        cur.execute("SELECT counter FROM row_counters WHERE (\
-                table_name = 'last_31_days');")
-
-        counter = cur.fetchone()[0]
-
-        cur.execute("SELECT SUM(energy) FROM last_24_hours;")
-
-        energy_total = cur.fetchone()[0]
-
-        conn.execute("UPDATE last_31_days SET energy = " + str(energy_total) + ", \
-                time = datetime(CURRENT_TIMESTAMP) \
-                WHERE (rowID = " + str(counter) + ");")
-        
-        if counter >= 31:
-                conn.execute("UPDATE row_counters SET counter = 1 \
-                        WHERE (table_name = 'last_31_days');")
-        else:
-                conn.execute("UPDATE row_counters SET counter = " + str(counter + 1) + " \
-                        WHERE (table_name = 'last_31_days');")
-        
-        conn.commit()
-        conn.close()
-
-def monthly():
-        conn = sqlite3.connect('test.db')
-        cur = conn.cursor()
-        cur.execute("SELECT SUM(energy) FROM last_31_days WHERE (time > datetime(CURRENT_TIMESTAMP, '-1 month'));")
-
-        energy_total = cur.fetchone()[0]
-
-        conn.execute("INSERT INTO montly_record VALUES (datetime(CURRENT_TIMESTAMP);")
+        conn.execute("INSERT INTO hourly_record VALUES (datetime(CURRENT_TIMESTAMP);")
         conn.commit()
         conn.close()
 
@@ -167,7 +71,3 @@ if __name__ == "__main__":
 		setup()
         elif sys.argv[1] == "hourly":
                 hourly()
-        elif sys.argv[1] == "daily":
-                daily()
-        elif sys.argv[1] == "monthly":
-                monthly()
